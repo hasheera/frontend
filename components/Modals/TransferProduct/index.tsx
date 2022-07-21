fa5dbd9e18871c19686738ca19d57a0403e5fe36
I-GiM <ikennaoyiih@gmail.com>
2022-07-21 08:05:11 +0100

    fix stock transfer qty exceeding stock

--- components/Modals/TransferProduct/index.tsx
+++ components/Modals/TransferProduct/index.tsx
@@ -108,7 +108,12 @@ const Transfer = ({ isOpen, onClose }: Props) => {
         <Input
           placeholder="Enter Quantity"
           value={qty}
-          onChange={(e: any) => setQty(e.target.value)}
+          onChange={(e: any) => {
+            if(Number(e.target.value) > singleProduct.stock_count) {
+              return setQty(singleProduct.stock_count)
+            };
+            return setQty(e.target.value);
+          }}
         />
         <InputRightElement>
           <chakra.button px="30px" onClick={() => setQty((num) => num + 1)}>
