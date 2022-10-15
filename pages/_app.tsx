74346d58750532cb719f40c2b24b420ea9669e9c
I-GiM <ikennaoyiih@gmail.com>
2022-10-15 10:57:00 +0100

    Initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,15 +5,12 @@ import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { Provider } from 'react-redux'
 import { store } from 'store'
-import GlobalFunctionsProvider from 'provider/GlobalFunctionsProvider'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
       <Provider store={store}>
-        <GlobalFunctionsProvider>
-          <Component {...pageProps} />
-        </GlobalFunctionsProvider>
+        <Component {...pageProps} />
       </Provider>
     </ChakraProvider>
   )
